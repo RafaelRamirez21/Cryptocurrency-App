5bd721035e88963b9965f69c8e6a31b93578b651
Rafael Ramírez <rrramirezb251@gmail.com>
2021-09-26 19:17:43 -0500

    component News added

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
+import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
+import { useGetCryptoNewsQuery } from '../../Services/cryptoNewsApi';
+import moment from 'moment';
+import '../../assets/styles/News.scss'
+const { Text, Title } = Typography;
+const { Option } = Select;
+const demoImage = 'http://coinrevolution.com/wp-content/uploads/2020/06/cryptonews.jpg'
+const News = ({ simplified }) => {
+  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrencies', count: simplified ? 6 : 13 })
+  if (!cryptoNews?.value) return 'Loading...'
 
-const News = () => {
   return (
-    <div>
+    <Row gutter={[24, 24]}>
+      {cryptoNews.value.map((news, index) => (
+        <Col xs={24} sm={12} lg={8} key={index}>
+          <Card hoverable className='news-card'>
+            <a href={news.url} target='_blank' rel="noreferrer">
+              <div className='news-image-container'>
+                <Title className='news-title' level={5}>
+                  {news.name}
+                </Title>
+                <img style={{ maxWidth: '200px', maxHeight: '100px' }} src={news?.image?.thumbnail?.contentUrl || demoImage} alt={news.name} />
+              </div>
+              <p>
+                {news.description > 100 ? `${news.description.substring(0, 100)}...`
+                  : news.description
+                }
+              </p>
+              <div className='provider-container'>
+                <div>
+                  <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl || demoImage} alt='news' />
+                  <Text className='provider-name'>{news.provider[0]?.name}</Text>
+                </div>
+                <Text>{moment(news.datePublished).startOf('ss').fromNow()}</Text>
 
-    </div>
+
+              </div>
+            </a>
+          </Card>
+
+        </Col>
+      ))}
+
+    </Row>
   )
 }
 
